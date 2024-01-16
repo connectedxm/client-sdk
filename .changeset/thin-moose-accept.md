3b7f67d37262d2d6e4d054ddf839aa6c86475559
Rob Upchurch <rob@connectedxm.com>
2024-01-16 15:02:24 -0700

    add getErrorMessage utility

---
+++ .changeset/thin-moose-accept.md
@@ -0,0 +1,5 @@
+---
+"@connectedxm/client": patch
+---
+
+add getErrorMessage utility
---
+++ src/utilities/GetErrorMessage.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { ConnectedXMResponse } from "../interfaces";
+
+const GetErrorMessage = (
+  error: any,
+  fallback: string = "Something went wrong"
+) => {
+  let message: string = fallback;
+  if (axios.isAxiosError(error)) {
+    message =
+      (error.response?.data as ConnectedXMResponse<any>)?.message || message;
+  } else {
+    message = (error as Error).message;
+  }
+
+  return message;
+};
+
+export default GetErrorMessage;
