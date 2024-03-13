74fa4ae42c9754c633cafd0238dfd0ca8ffe44c6
joshuackeller <joshuackeller@gmail.com>
2024-03-13 12:35:13 -0600

    invoices

--- src/mutations/index.ts
+++ src/mutations/index.ts
@@ -2,6 +2,7 @@ export * from "./accounts";
 export * from "./activities";
 export * from "./communityModerator";
 export * from "./events";
+export * from "./invoices";
 export * from "./self";
 export * from "./supportTicket";
 export * from "./team";
