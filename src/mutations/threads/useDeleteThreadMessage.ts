62f6886c9430c520fceb1c1ddc745762bd977d6a
daniel <daniel@connectedxm.com>
2024-07-02 08:24:23 -0500

    chore: Add useDeleteThreadMessage mutation and related functions

---
+++ src/mutations/threads/useDeleteThreadMessage.ts
@@ -0,0 +1,54 @@
+import { GetClientAPI } from "@src/ClientAPI";
+import { ConnectedXMResponse } from "@src/interfaces";
+import useConnectedMutation, {
+  MutationOptions,
+  MutationParams,
+} from "@src/mutations/useConnectedMutation";
+import { THREAD_MESSAGES_QUERY_KEY } from "@src/queries";
+
+export interface DeleteThreadMessageParams extends MutationParams {
+  threadId: string;
+  messageId: string;
+  body: string;
+  moderator?: boolean;
+}
+
+export const DeleteThreadMessage = async ({
+  threadId,
+  messageId,
+  body,
+  moderator,
+  clientApiParams,
+  queryClient,
+}: DeleteThreadMessageParams): Promise<ConnectedXMResponse<null>> => {
+  const clientApi = await GetClientAPI(clientApiParams);
+  const { data } = await clientApi.delete<ConnectedXMResponse<null>>(
+    `/threads/${threadId}/messages/${messageId}`,
+    {
+      data: { body, moderator },
+    }
+  );
+
+  if (queryClient && data.status === "ok") {
+    queryClient.invalidateQueries({
+      queryKey: THREAD_MESSAGES_QUERY_KEY(threadId),
+    });
+  }
+
+  return data;
+};
+
+export const useDeleteThreadMessage = (
+  options: Omit<
+    MutationOptions<
+      Awaited<ReturnType<typeof DeleteThreadMessage>>,
+      Omit<DeleteThreadMessageParams, "queryClient" | "clientApiParams">
+    >,
+    "mutationFn"
+  > = {}
+) => {
+  return useConnectedMutation<
+    DeleteThreadMessageParams,
+    Awaited<ReturnType<typeof DeleteThreadMessage>>
+  >(DeleteThreadMessage, options);
+};
