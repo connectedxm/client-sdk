98059dfbc692b43b94b76b464b063f2ed79e29db
daniel <daniel@connectedxm.com>
2024-07-02 08:24:45 -0500

    feat: Implement useAddThreadMember mutation and related functions

---
+++ src/mutations/threads/useAddThreadMember.ts
@@ -0,0 +1,47 @@
+import { GetClientAPI } from "@src/ClientAPI";
+import { ConnectedXMResponse, ThreadMember } from "@src/interfaces";
+import useConnectedMutation, {
+  MutationOptions,
+  MutationParams,
+} from "@src/mutations/useConnectedMutation";
+import { THREAD_MEMBERS_QUERY_KEY } from "@src/queries";
+
+export interface AddThreadMemberParams extends MutationParams {
+  threadId: string;
+  accountId: string;
+}
+
+export const AddThreadMember = async ({
+  threadId,
+  accountId,
+  clientApiParams,
+  queryClient,
+}: AddThreadMemberParams): Promise<ConnectedXMResponse<ThreadMember>> => {
+  const clientApi = await GetClientAPI(clientApiParams);
+  const { data } = await clientApi.post<ConnectedXMResponse<ThreadMember>>(
+    `/threads/${threadId}/members/${accountId}`
+  );
+
+  if (queryClient && data.status === "ok") {
+    queryClient.invalidateQueries({
+      queryKey: THREAD_MEMBERS_QUERY_KEY(threadId),
+    });
+  }
+
+  return data;
+};
+
+export const useAddThreadMember = (
+  options: Omit<
+    MutationOptions<
+      Awaited<ReturnType<typeof AddThreadMember>>,
+      Omit<AddThreadMemberParams, "queryClient" | "clientApiParams">
+    >,
+    "mutationFn"
+  > = {}
+) => {
+  return useConnectedMutation<
+    AddThreadMemberParams,
+    Awaited<ReturnType<typeof AddThreadMember>>
+  >(AddThreadMember, options);
+};
