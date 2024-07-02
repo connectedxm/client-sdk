17ab2600b775f3a8a011ac19ce1897fc60abc681
daniel <daniel@connectedxm.com>
2024-07-02 08:23:23 -0500

    chore: Add new thread-related queries and update useGetThread

--- src/queries/threads/index.ts
+++ src/queries/threads/index.ts
@@ -1,2 +1,6 @@
 export * from "./useGetThreads";
 export * from "./useGetThread";
+export * from "./useGetThreadMember";
+export * from "./useGetThreadMembers";
+export * from "./useGetThreadMessages";
+export * from "./useGetThreadMessage";
--- src/queries/threads/useGetThread.ts
+++ src/queries/threads/useGetThread.ts
@@ -7,19 +7,24 @@ import {
 
 import type { Thread } from "@interfaces";
 import { THREADS_QUERY_KEY } from "./useGetThreads";
-import { QueryClient, SetDataOptions, QueryKey } from "@tanstack/react-query";
+import {
+  QueryClient,
+  SetDataOptions,
+  QueryKey,
+  Updater,
+} from "@tanstack/react-query";
 import { ConnectedXMResponse } from "@interfaces";
 import { GetClientAPI } from "@src/ClientAPI";
 
-export const THREAD_QUERY_KEY = (threadId: string): QueryKey => [
-  ...THREADS_QUERY_KEY(),
-  threadId,
-];
+export const THREAD_QUERY_KEY = (
+  threadId: string,
+  accountId?: string
+): QueryKey => [...THREADS_QUERY_KEY(), threadId, accountId];
 
 export const SET_THREAD_QUERY_DATA = (
   client: QueryClient,
   keyParams: Parameters<typeof THREAD_QUERY_KEY>,
-  response: Awaited<ReturnType<typeof GetThread>>,
+  response: Updater<any, Awaited<ReturnType<typeof GetThread>>>,
   baseKeys: Parameters<typeof GetBaseSingleQueryKeys> = ["en"],
   options?: SetDataOptions
 ) => {
