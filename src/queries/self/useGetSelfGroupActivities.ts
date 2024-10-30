90805a5afc917a8fa904f0955ebbf304f507c35b
joshuackeller <joshuackeller@gmail.com>
2024-10-30 09:20:22 -0600

    fix group activities query

--- src/queries/self/useGetSelfGroupActivities.ts
+++ src/queries/self/useGetSelfGroupActivities.ts
@@ -4,13 +4,13 @@ import {
   InfiniteQueryParams,
   useConnectedInfiniteQuery,
 } from "../useConnectedInfiniteQuery";
-import { SELF_QUERY_KEY } from "./useGetSelf";
 import { QueryKey } from "@tanstack/react-query";
 import { GetClientAPI } from "@src/ClientAPI";
 import { useConnectedXM } from "@src/hooks";
+import { ACTIVITIES_QUERY_KEY } from "../activities";
 
 export const SELF_GROUP_ACTIVITIES_QUERY_KEY = (): QueryKey => [
-  ...SELF_QUERY_KEY(),
+  ...ACTIVITIES_QUERY_KEY(),
   "GROUP_ACTIVITIES",
 ];
 
