42a3316a91b80ee45bacf9693761c8dc50c6b302
joshuackeller <joshuackeller@gmail.com>
2024-11-25 14:10:27 -0700

    wip

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -958,12 +958,19 @@ export interface SessionQuestionResponse extends BaseSessionQuestionResponse {
   updatedAt: string;
 }
 
+export enum SessionPassStatus {
+  draft = "draft",
+  ready = "ready",
+  canceled = "canceled",
+}
+
 export interface BaseSessionPass {
   id: string;
   status: string;
 }
 
 export interface SessionPass extends BaseSessionPass {
+  pass: BasePurchase;
   createdAt: string;
   updatedAt: string;
 }
