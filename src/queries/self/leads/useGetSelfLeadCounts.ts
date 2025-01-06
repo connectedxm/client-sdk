274790b3b1bd19a166f7d61f689fbef98f5aaed0
Rob Upchurch <robertc.upchurch@gmail.com>
2025-01-06 14:36:33 -0700

    Remove state and country from Lead interface; add useGetSelfLeadCounts query

---
+++ src/queries/self/leads/useGetSelfLeadCounts.ts
@@ -0,0 +1,45 @@
+import {
+  SingleQueryOptions,
+  SingleQueryParams,
+  useConnectedSingleQuery,
+} from "../../useConnectedSingleQuery";
+
+import type { ConnectedXMResponse, LeadStatus } from "@interfaces";
+import { QueryKey } from "@tanstack/react-query";
+import { GetClientAPI } from "@src/ClientAPI";
+import { useConnectedXM } from "@src/hooks";
+import { SELF_LEADS_QUERY_KEY } from "./useGetSelfLeads";
+
+export const SELF_LEAD_COUNTS_QUERY_KEY = (): QueryKey => {
+  const keys = [...SELF_LEADS_QUERY_KEY(), "COUNTS"];
+  return keys;
+};
+
+export interface GetSelfLeadCountsProps extends SingleQueryParams {}
+
+export const GetSelfLeadCounts = async ({
+  clientApiParams,
+}: GetSelfLeadCountsProps): Promise<
+  ConnectedXMResponse<Record<LeadStatus, number>>
+> => {
+  const clientApi = await GetClientAPI(clientApiParams);
+
+  const { data } = await clientApi.get(`/self/leads/counts`);
+
+  return data;
+};
+
+export const useGetSelfLeadCounts = (
+  options: SingleQueryOptions<ReturnType<typeof GetSelfLeadCounts>> = {}
+) => {
+  const { authenticated } = useConnectedXM();
+
+  return useConnectedSingleQuery<ReturnType<typeof GetSelfLeadCounts>>(
+    SELF_LEAD_COUNTS_QUERY_KEY(),
+    (params: SingleQueryParams) => GetSelfLeadCounts({ ...params }),
+    {
+      ...options,
+      enabled: !!authenticated && (options?.enabled ?? true),
+    }
+  );
+};
