e06d5960d1fc23092b41b05fc27289cd603df4d8
Spencer Fund <fund.spencer@gmail.com>
2025-08-27 17:29:55 -0600

    Update 'past' parameter type to boolean in useGetAllGroupEvents for improved type consistency and clarity.

--- src/queries/groups/useGetAllGroupEvents.ts
+++ src/queries/groups/useGetAllGroupEvents.ts
@@ -11,7 +11,7 @@ import { ConnectedXMResponse } from "@interfaces";
 import { GetClientAPI } from "@src/ClientAPI";
 import { EVENTS_QUERY_KEY } from "../events";
 
-export const ALL_GROUP_EVENTS = (past?: true): QueryKey => [
+export const ALL_GROUP_EVENTS = (past?: boolean): QueryKey => [
   ...EVENTS_QUERY_KEY(past),
   "GROUPS_EVENTS",
 ];
@@ -57,7 +57,7 @@ export const GetAllGroupEvents = async ({
 };
 
 export const useGetAllGroupEvents = (
-  past?: true,
+  past: boolean = false,
   params: Omit<
     InfiniteQueryParams,
     "pageParam" | "queryClient" | "clientApiParams"
