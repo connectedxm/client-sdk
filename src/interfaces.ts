31347aea289bcf617d0a6971be16222ccb10061f
Spencer Fund <fund.spencer@gmail.com>
2025-09-29 15:04:06 -0600

    Update version to 6.4.3 in package.json and package-lock.json; add masked property to BaseRegistrationQuestion, BaseEventSessionQuestion, and BaseSurveyQuestion interfaces for improved data handling.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -532,6 +532,7 @@ export interface BaseRegistrationQuestion {
   mutable: boolean;
   min: string | null;
   max: string | null;
+  masked: boolean;
   validation: string | null;
   validationMessage: string | null;
   locationOption: LocationQuestionOption | null;
@@ -1129,6 +1130,7 @@ export interface BaseEventSessionQuestion {
   mutable: boolean;
   min: string | null;
   max: string | null;
+  masked: boolean;
   validation: string | null;
   validationMessage: string | null;
   locationOption: LocationQuestionOption | null;
@@ -2751,6 +2753,7 @@ export interface BaseSurveyQuestion {
   mutable: boolean;
   min: string | null;
   max: string | null;
+  masked: boolean;
   validation: string | null;
   validationMessage: string | null;
   locationOption: LocationQuestionOption | null;
