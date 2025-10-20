4207d35d52018477fd0d9903b9c2b6b29d6a1328
Spencer Fund <fund.spencer@gmail.com>
2025-10-20 16:25:38 -0600

    Remove useUpdateActivity mutation and its export from activities index

--- src/mutations/activities/index.ts
+++ src/mutations/activities/index.ts
@@ -1,5 +1,4 @@
 export * from "./useCreateActivity";
-export * from "./useUpdateActivity";
 export * from "./useDeleteActivity";
 export * from "./useLikeActivity";
 export * from "./useUnlikeActivity";
