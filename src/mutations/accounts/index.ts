d4f20fce532b75fbb8dec04fe27e539c1f1779f4
Spencer Fund <fund.spencer@gmail.com>
2025-10-27 09:56:06 -0600

    Remove useUpdateAccount mutation and its export from accounts index

--- src/mutations/accounts/index.ts
+++ src/mutations/accounts/index.ts
@@ -2,4 +2,3 @@ export * from "./useFollowAccount";
 export * from "./useUnfollowAccount";
 export * from "./useBlockAccount";
 export * from "./useUnblockAccount";
-export * from "./useUpdateAccount";
