2431357aa56890485c98ba2da8dfc38dcbe2577a
swiftoO <43972835+swiftoO@users.noreply.github.com>
2026-02-13 10:44:24 -0700

    Add useCancelSelfEventSessionAccess hook and update index export

---
+++ src/mutations/self/events/attendee/sessions/useCancelSelfEventSessionAccess.ts
@@ -0,0 +1,67 @@
+import { ConnectedXMResponse, EventSessionAccess } from "@src/interfaces";
+import useConnectedMutation, {
+  MutationOptions,
+  MutationParams,
+} from "../../../../useConnectedMutation";
+import { GetClientAPI } from "@src/ClientAPI";
+import {
+  SELF_EVENT_ATTENDEE_ACCESS_QUERY_KEY,
+  SELF_EVENT_ATTENDEE_QUERY_KEY,
+} from "@src/queries";
+
+export interface CancelSelfEventSessionAccessParams extends MutationParams {
+  eventId: string;
+  passId: string;
+  sessionId: string;
+  sendEmail?: boolean;
+}
+
+export const CancelSelfEventSessionAccess = async ({
+  eventId,
+  passId,
+  sessionId,
+  sendEmail = true,
+  clientApiParams,
+  queryClient,
+}: CancelSelfEventSessionAccessParams): Promise<
+  ConnectedXMResponse<EventSessionAccess>
+> => {
+  const clientApi = await GetClientAPI(clientApiParams);
+  const { data } = await clientApi.put<ConnectedXMResponse<EventSessionAccess>>(
+    `/self/events/${eventId}/attendee/passes/${passId}/sessions/${sessionId}/cancel`,
+    { sendEmail }
+  );
+
+  if (queryClient && data.status === "ok") {
+    queryClient.invalidateQueries({
+      queryKey: SELF_EVENT_ATTENDEE_ACCESS_QUERY_KEY(
+        eventId,
+        passId,
+        sessionId
+      ),
+    });
+    queryClient.invalidateQueries({
+      queryKey: SELF_EVENT_ATTENDEE_QUERY_KEY(eventId),
+    });
+  }
+
+  return data;
+};
+
+export const useCancelSelfEventSessionAccess = (
+  options: Omit<
+    MutationOptions<
+      Awaited<ReturnType<typeof CancelSelfEventSessionAccess>>,
+      Omit<
+        CancelSelfEventSessionAccessParams,
+        "queryClient" | "clientApiParams"
+      >
+    >,
+    "mutationFn"
+  > = {}
+) => {
+  return useConnectedMutation<
+    CancelSelfEventSessionAccessParams,
+    Awaited<ConnectedXMResponse<EventSessionAccess>>
+  >(CancelSelfEventSessionAccess, options);
+};
