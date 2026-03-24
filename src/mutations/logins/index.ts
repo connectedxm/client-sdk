06e191c11db98472a59079ed352274e3ee1536de
swiftoO <43972835+swiftoO@users.noreply.github.com>
2026-03-24 13:21:15 -0600

    feat: Add useCheckLogin export to login mutations

--- src/mutations/logins/index.ts
+++ src/mutations/logins/index.ts
@@ -1,4 +1,5 @@
 export * from "./useCreateLoginAccount";
+export * from "./useCheckLogin";
 export * from "./useVerifyLoginAccount";
 export * from "./useAddLogin";
 export * from "./useRemoveLogin";
---
+++ src/mutations/logins/useCheckLogin.ts
@@ -0,0 +1,38 @@
+import { ConnectedXMResponse } from "@src/interfaces";
+import useConnectedMutation, {
+  MutationOptions,
+  MutationParams,
+} from "../useConnectedMutation";
+import { GetClientAPI } from "@src/ClientAPI";
+
+export interface CheckLoginParams extends MutationParams {
+  email: string;
+}
+
+export const CheckLogin = async ({
+  email,
+  clientApiParams,
+}: CheckLoginParams): Promise<ConnectedXMResponse<boolean>> => {
+  const clientApi = await GetClientAPI(clientApiParams);
+  const { data } = await clientApi.post<ConnectedXMResponse<boolean>>(
+    `/login/check`,
+    { email }
+  );
+
+  return data;
+};
+
+export const useCheckLogin = (
+  options: Omit<
+    MutationOptions<
+      Awaited<ReturnType<typeof CheckLogin>>,
+      Omit<CheckLoginParams, "queryClient" | "clientApiParams">
+    >,
+    "mutationFn"
+  > = {}
+) => {
+  return useConnectedMutation<
+    CheckLoginParams,
+    Awaited<ReturnType<typeof CheckLogin>>
+  >(CheckLogin, options);
+};
